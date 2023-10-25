c5f08723aa958c8a815343c5f1612761c2245dd6
julian <julian@polluxcoop.com>
2023-10-25 17:24:06 -0300

    feat: add dashboard template

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // output: "export",
-};
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid",
+  "react-github-btn",
+]);
 
-module.exports = nextConfig;
+module.exports = withTM({
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboards/analytics",
+        permanent: true,
+      },
+    ];
+  },
+});

60e91530c73f415f069e8a9820323388910b6114
soundOfff <tomas@polluxcoop.com>
2024-05-08 15:33:54 -0400

    wip duplicate taxes on items

--- front/actions/items.js
+++ front/actions/items.js
@@ -1,7 +1,6 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 import { customFetch } from "./custom-fetch";
 
 export async function getAll(params) {

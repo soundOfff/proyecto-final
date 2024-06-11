5ccfdbc0decd697a0c7efb6ac8559503fafb1848
soundOfff <tomas@polluxcoop.com>
2024-06-11 19:01:21 -0400

    hotfix login v2

--- front/app/api/auth/logout/route.js
+++
@@ -1,9 +0,0 @@
-import { logout } from "/actions/staffs";
-
-export async function POST(request) {
-  const body = await request.json();
-
-  await logout(body.staff_id);
-
-  return Response.json({ message: "Usuario deslogeado correctamente" });
-}

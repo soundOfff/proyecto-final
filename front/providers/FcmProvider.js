7c6829c14df197b437f4f24e34448775bcc0d23b
julianvilloria <julian@polluxcoop.com>
2024-06-18 17:14:33 -0300

    feat: add dialog to accept web push notifications

--- front/providers/FcmProvider.js
+++ front/providers/FcmProvider.js
@@ -1,16 +1,53 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getMessaging, onMessage } from "firebase/messaging";
-import firebaseApp from "/utils/firebase/index";
-import useFcm from "/hooks/useFcm";
 import MDSnackbar from "/components/MDSnackbar";
 import { useSession } from "next-auth/react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import MDButton from "/components/MDButton";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import firebaseApp from "/utils/firebase/index";
+import { storeToken } from "/actions/fcm";
 
 export default function FCM({ children }) {
   const [payload, setPayload] = useState(null);
+  const [token, setToken] = useState("");
+  const [permission, setPermission] = useState(Notification.permission);
+  const [open, setOpen] = useState(false);
   const { data: session } = useSession();
-  const { token, permission } = useFcm({ session });
+
+  const handleAccept = () => {
+    if (window !== "undefined") {
+      const handler = () => setPermission(Notification.permission);
+      handler();
+
+      Notification.requestPermission().then(handler);
+
+      navigator.permissions
+        .query({ name: "notifications" })
+        .then((notificationPermission) => {
+          notificationPermission.onchange = handler;
+        });
+      setOpen(false);
+    }
+  };
+
+  const handleReject = () => {
+    setPermission("denied");
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    if (permission === "default") {
+      setOpen(true);
+    }
+  }, [permission]);
 
   useEffect(() => {
     if (typeof window !== "undefined" && "serviceWorker" in navigator) {
@@ -26,6 +63,42 @@ export default function FCM({ children }) {
     }
   }, [token, permission, session]);
 
+  useEffect(() => {
+    const retrieveToken = async () => {
+      try {
+        if (
+          typeof window !== "undefined" &&
+          "serviceWorker" in navigator &&
+          session
+        ) {
+          const register = await navigator.serviceWorker.register(
+            "/firebase-messaging-sw.js"
+          );
+          const messaging = getMessaging(firebaseApp);
+
+          // Check if permission is granted before retrieving the token
+          if (permission === "granted") {
+            const currentToken = await getToken(messaging, {
+              vapidKey:
+                "BN9siHaVjvL_wBx5vx0_zP7BI8mEwwLqOfzAVztlojc9fenuoWkkLc5qNtGy-HXnnglT3HUmILiAvvWQJY5GK-c",
+              serviceWorkerRegistration: register,
+            });
+            storeToken({
+              device_token: currentToken,
+              staff_id: session.staff.id,
+            });
+            if (currentToken) {
+              setToken(currentToken);
+            }
+          }
+        }
+      } catch (error) {
+        console.log("An error occurred while retrieving token:", error);
+      }
+    };
+    retrieveToken();
+  }, [permission, session]);
+
   return (
     <>
       {payload && (
@@ -40,6 +113,30 @@ export default function FCM({ children }) {
           bgWhite
         />
       )}
+      <Dialog
+        open={open}
+        onClose={handleReject}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+        sx={{ p: 5 }}
+      >
+        <DialogTitle id="alert-dialog-title">
+          ¿Le gustaría recibir notificaciones?
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Se le notificará si tiene un recordatorio programado
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <MDButton onClick={handleAccept} color="success">
+            Aceptar
+          </MDButton>
+          <MDButton onClick={handleReject} color="error">
+            Rechazar
+          </MDButton>
+        </DialogActions>
+      </Dialog>
       {children}
     </>
   );

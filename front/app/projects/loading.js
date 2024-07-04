5fef3936b8c6cb01f610d30197912508284ff996
julianvilloria <julian@polluxcoop.com>
2024-07-04 14:10:34 -0300

    fix: unify project loader

--- front/app/projects/loading.js
+++ front/app/projects/loading.js
@@ -1,21 +1,5 @@
-import { Card, Grid } from "@mui/material";
-import MDBox from "/components/MDBox";
-import StatsLoading from "./components/loading/stats";
-import FiltersLoading from "./components/loading/filters";
-import TableLoading from "./components/loading/table";
+import Loader from "/components/Loader";
 
 export default function Loading() {
-  return (
-    <MDBox mb={3}>
-      <Card>
-        <Grid container spacing={3} p={5}>
-          <Grid item xs={12}>
-            <StatsLoading />
-            <FiltersLoading />
-            <TableLoading />
-          </Grid>
-        </Grid>
-      </Card>
-    </MDBox>
-  );
+  return <Loader />;
 }

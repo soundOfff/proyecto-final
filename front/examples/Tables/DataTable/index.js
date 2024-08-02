161da14a1500f30d80d585ef4ce498ba785c0f97
Abril Rodriguez Lijovit <abrurodriguez0105@gmail.com>
2024-08-02 12:00:51 -0300

    change pagination message to spanish

--- front/examples/Tables/DataTable/index.js
+++ front/examples/Tables/DataTable/index.js
@@ -248,7 +248,7 @@ function DataTable({
                 renderInput={(params) => <MDInput {...params} />}
               />
               <MDTypography variant="caption" color="secondary">
-                &nbsp;&nbsp;entries per page
+                &nbsp;&nbsp;entradas por página
               </MDTypography>
             </MDBox>
           )}

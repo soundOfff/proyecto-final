45a21ed69997a07de6023fc135f34d3931f73042
julianvilloria <julian@polluxcoop.com>
2024-09-09 17:10:13 -0300

    fix: avoid duplicated web push notification on service worker

--- front/public/firebase-messaging-sw.js
+++ front/public/firebase-messaging-sw.js
@@ -17,15 +17,15 @@ firebase.initializeApp(firebaseConfig);
 // eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: "./logo.png",
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// messaging.onBackgroundMessage((payload) => {
+//   console.log(
+//     "[firebase-messaging-sw.js] Received background message ",
+//     payload
+//   );
+//   const notificationTitle = payload.notification.title;
+//   const notificationOptions = {
+//     body: payload.notification.body,
+//     icon: "./logo.png",
+//   };
+//   self.registration.showNotification(notificationTitle, notificationOptions);
+// });

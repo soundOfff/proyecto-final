f142c5bccd791e39bd89ff6801a03c3f9e11259b
JulianVilloria <101416753+JulianVilloria@users.noreply.github.com>
2024-09-16 11:33:33 -0300

    fix: add nullable to description in court table

--- back/database/migrations/2024_13_29_540741_create_courts_table.php
+++ back/database/migrations/2024_13_29_540741_create_courts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->string('number');
-            $table->string('description');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }

870c989970220d7ca441175c1348e5539e95ba9a
fbiondi <fdbion@gmail.com>
2024-09-17 10:00:43 -0300

    refactor partners stats

--- front/app/partners/components/stats.js
+++ front/app/partners/components/stats.js
@@ -3,129 +3,75 @@ import MDBox from "/components/MDBox";
 import MDTypography from "/components/MDTypography";
 import MDBadge from "/components/MDBadge";
 
+export function StatBox({ label, count, color, xs = 12, lg = 4 }) {
+  return (
+    <Grid item xs={xs} lg={lg}>
+      <MDBox
+        display="flex"
+        my={0.5}
+        mx={{ xs: "10vw", sm: "15vw", md: 0 }}
+        gap={0.5}
+      >
+        <MDTypography
+          variant="h6"
+          display="inline-block"
+          minWidth="45px"
+          textAlign="end"
+        >
+          {count}
+        </MDTypography>
+        <MDBadge
+          variant="contained"
+          badgeContent={label}
+          color={color}
+          size="xs"
+          container
+          sx={{ ml: 1, height: "2rem" }}
+        />
+      </MDBox>
+    </Grid>
+  );
+}
+
 export default function Stats({ stats }) {
   return (
     <Grid container sx={{ mx: { xs: 0, sm: 2 } }}>
       <Grid item xs={12} md={6} lg={12}>
         <Grid container>
-          <Grid item xs={12} lg={4}>
-            <MDBox display="flex" my={2} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.partner.total}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Total de Clientes"
-                color="dark"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem" }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} lg={4}>
-            <MDBox display="flex" my={2} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.partner.active}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Clientes Activos"
-                color="success"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem" }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} lg={4} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-            <MDBox display="flex" my={2}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.partner.inactive}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Clientes inactivos"
-                color="error"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem", width: "200px" }}
-              />
-            </MDBox>
-          </Grid>
+          <StatBox
+            label="Total de Clientes"
+            count={stats.partner.total}
+            color="dark"
+          />
+          <StatBox
+            label="Clientes Activos"
+            count={stats.partner.active}
+            color="success"
+          />
+          <StatBox
+            label="Clientes Inactivos"
+            count={stats.partner.inactive}
+            color="error"
+          />
         </Grid>
       </Grid>
       <Grid item xs={12} md={6} lg={12}>
         <Grid container>
-          <Grid item xs={12} lg={4}>
-            <MDBox display="flex" my={2} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.contact.total}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Total de Contactos"
-                color="dark"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem", width: "200px" }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} lg={4}>
-            <MDBox display="flex" my={2} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.contact.active}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Contactos Activos"
-                color="success"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem", width: "200px" }}
-              />
-            </MDBox>
-          </Grid>
-          <Grid item xs={12} lg={4}>
-            <MDBox display="flex" my={2} mx={{ xs: "10vw", sm: "15vw", md: 0 }}>
-              <MDTypography
-                variant="h3"
-                display="inline-block"
-                minWidth="100px"
-              >
-                {stats.contact.inactive}
-              </MDTypography>
-              <MDBadge
-                variant="contained"
-                badgeContent="Contactos inactivos"
-                color="error"
-                size="xs"
-                container
-                sx={{ ml: 1, height: "2rem", width: "200px" }}
-              />
-            </MDBox>
-          </Grid>
+          <StatBox
+            label="Total de Contactos"
+            count={stats.contact.total}
+            color="dark"
+          />
+          <StatBox
+            label="Contactos Activos"
+            count={stats.contact.active}
+            color="success"
+          />
+          <StatBox
+            label="Contactos Inactivos"
+            count={stats.contact.inactive}
+            color="error"
+          />
         </Grid>
       </Grid>
     </Grid>

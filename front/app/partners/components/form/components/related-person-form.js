fe39e32a53317ba1aa5a3f6f5aeb4cdb4b36c24e
soundOfff <tomas@polluxcoop.com>
2024-09-20 15:40:15 -0400

    wip: add edit button and modal to attach info

--- front/app/partners/components/form/components/related-person-form.js
+++ front/app/partners/components/form/components/related-person-form.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { useFormik } from "formik";
+import moment from "moment";
+
 import { Grid, Icon, Switch, FormControlLabel, FormGroup } from "@mui/material";
 import MDButton from "/components/MDButton";
 import Select from "/components/Select";
@@ -8,11 +11,9 @@ import MDTypography from "/components/MDTypography";
 import MDDatePicker from "/components/MDDatePicker";
 import Modal from "/components/Modal";
 import PersonForm from "/components/ModalContent/Partner/";
-import FormField from "./form-field";
 
 import * as Yup from "yup";
-import { useFormik } from "formik";
-import moment from "moment";
+import OwnerForm from "./owner-person-form";
 
 const newRelatedPeopleFormField = {
   formId: "new-related-people",
@@ -87,6 +88,7 @@ export default function RelatedPersonFormComponent({
   partnerTypes,
 }) {
   const [openModal, setOpenModal] = useState(false);
+  const [ownerModalOpen, setOwnerModalOpen] = useState(true);
   const {
     relatedPartnerId,
     partnerTypeId,
@@ -298,7 +300,7 @@ export default function RelatedPersonFormComponent({
           </MDTypography>
         </MDBox>
       </Grid>
-      <Grid xs={12} sm={3} item>
+      <Grid xs={12} sm={2} item>
         <Select
           value={values[partnerTypeId.name]}
           options={partnerTypes}
@@ -365,7 +367,7 @@ export default function RelatedPersonFormComponent({
         </MDBox>
       </Grid>
       <Grid xs={12} sm={2} item>
-        <FormGroup>
+        <FormGroup mx="auto">
           <FormControlLabel
             control={
               <Switch
@@ -377,108 +379,16 @@ export default function RelatedPersonFormComponent({
           />
         </FormGroup>
       </Grid>
-      <Grid item xs={12} sm={2}>
-        <FormField
-          name={seat.name}
-          label={seat.label}
-          type={seat.type}
-          setFieldValue={setFieldValue}
-          value={values[seat.name]}
-          error={errors[seat.name] && touched[seat.name]}
-          success={values[seat.name]?.length > 0 && !errors[seat.name]}
-        />
-      </Grid>
-      <Grid item xs={12} sm={2}>
-        <FormField
-          name={legalCircuit.name}
-          label={legalCircuit.label}
-          setFieldValue={setFieldValue}
-          type={legalCircuit.type}
-          value={values[legalCircuit.name]}
-          error={errors[legalCircuit.name] && touched[legalCircuit.name]}
-          success={
-            values[legalCircuit.name]?.length > 0 && !errors[legalCircuit.name]
-          }
-        />
-      </Grid>
-      <Grid xs={12} sm={2} item>
-        <MDDatePicker
-          input={{
-            fullWidth: false,
-            label: "Entrada",
-          }}
-          format="DD/MM/YYYY"
-          value={values[checkIn.name]}
-          onChange={(value) =>
-            setFieldValue(checkIn.name, moment(value[0]).format("YYYY-MM-DD"))
-          }
-        />
-        <MDBox mt={0.75}>
-          <MDTypography
-            component="div"
-            variant="caption"
-            color="error"
-            fontWeight="regular"
-          >
-            {errors[checkIn.name]}
-          </MDTypography>
-        </MDBox>
-      </Grid>
-      <Grid item xs={12} sm={2}>
-        <FormField
-          name={deed.name}
-          label={deed.label}
-          setFieldValue={setFieldValue}
-          type={deed.type}
-          value={values[deed.name]}
-          error={errors[deed.name] && touched[deed.name]}
-          success={values[deed.name]?.length > 0 && !errors[deed.name]}
-        />
-      </Grid>
-      <Grid xs={12} sm={2} item>
-        <MDDatePicker
-          input={{
-            fullWidth: false,
-            label: "Fecha de escritura",
-          }}
-          format="DD/MM/YYYY"
-          value={values[deedDate.name]}
-          onChange={(value) =>
-            setFieldValue(deedDate.name, moment(value[0]).format("YYYY-MM-DD"))
-          }
-        />
-        <MDBox mt={0.75}>
-          <MDTypography
-            component="div"
-            variant="caption"
-            color="error"
-            fontWeight="regular"
-          >
-            {errors[deedDate.name]}
-          </MDTypography>
-        </MDBox>
-      </Grid>
-      <Grid item xs={12} sm={1}>
-        <FormField
-          name={notary.name}
-          label={notary.label}
-          setFieldValue={setFieldValue}
-          type={notary.type}
-          value={values[notary.name]}
-          error={errors[notary.name] && touched[notary.name]}
-          success={values[notary.name]?.length > 0 && !errors[notary.name]}
-        />
-      </Grid>
-      <Grid item xs={12} sm={1}>
-        <FormField
-          name={sheet.name}
-          label={sheet.label}
-          setFieldValue={setFieldValue}
-          type={sheet.type}
-          value={values[sheet.name]}
-          error={errors[sheet.name] && touched[sheet.name]}
-          success={values[sheet.name]?.length > 0 && !errors[sheet.name]}
-        />
+      <Grid xs={12} sm={1} item>
+        <MDButton
+          variant="gradient"
+          color="dark"
+          size="small"
+          sx={{ width: "100%" }}
+          onClick={() => setOwnerModalOpen(true)}
+        >
+          Editar <Icon sx={{ mb: 1, ml: 1 }}>edit</Icon>
+        </MDButton>
       </Grid>
       <Grid xs={12} display="flex" gap={2} justifyContent="end" item>
         <MDButton
@@ -506,6 +416,20 @@ export default function RelatedPersonFormComponent({
           countries={countries}
           handleClose={() => setOpenModal(false)}
           setFieldValue={setFieldValue}
+        />
+      </Modal>
+      <Modal
+        open={ownerModalOpen}
+        onClose={() => setOwnerModalOpen(false)}
+        width="40%"
+      >
+        <OwnerForm
+          setFieldValue={setFieldValue}
+          values={values}
+          errors={errors}
+          handleCancel={() => setOwnerModalOpen(false)}
+          handleSubmit={handleSubmit}
+          formField={newRelatedPeopleFormField.formField}
         />
       </Modal>
     </>
